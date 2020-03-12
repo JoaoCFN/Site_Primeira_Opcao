2dc906ecd6b9ea2370affeb82f5e2893f4deca43
Genivaldo Alves <geniinho@hotmail.com>
2020-03-12 10:38:58 -0300

    Atualização dos pratos
    preciso de ajuda para diminuir o background para possibilitar o fechamento do modal. A navbar esta ficando por cima :baby_chick:

--- conteudo/js/app.js
+++ conteudo/js/app.js
@@ -1,89 +1,87 @@
 // FUNÇÃO QUE ANIMA A ROLAGEM DO SITE PARA BAIXO COM O CLIQUE DO BOTÃO
-	//FAZER TRANSIÇÃO SUAVE PELO SCROLL	
-function anima_rolagem(){
-	// FUNÇÃO QUE CAPTURA O CLICK DO LINK DO SPAN OU 'SPAN A'
-	$('span a').click(function(e) {
-		// E.PREVENT PREVINE ALGO. OU SEJA, QUANDO EU CLICAR NESSE BUTÃO A AÇÃO PADRÃO DELE NÃO ACONTECE
-		e.preventDefault();
-		// CAPTURO O ATRIBUTO HREF QUANDO O BUTÃO FOI CLICADO
-		// THIS VAI SER O ITEM QUE EU CLIQUEI
+//FAZER TRANSIÇÃO SUAVE PELO SCROLL	
+function anima_rolagem() {
+    // FUNÇÃO QUE CAPTURA O CLICK DO LINK DO SPAN OU 'SPAN A'
+    $('span a').click(function(e) {
+        // E.PREVENT PREVINE ALGO. OU SEJA, QUANDO EU CLICAR NESSE BUTÃO A AÇÃO PADRÃO DELE NÃO ACONTECE
+        e.preventDefault();
+        // CAPTURO O ATRIBUTO HREF QUANDO O BUTÃO FOI CLICADO
+        // THIS VAI SER O ITEM QUE EU CLIQUEI
 
-		var id = $(this).attr('href'),
-			// PEGA A DISTÂNCIA ENTRE O ELEMENTO CAPTURADO E O TOPO DA PÁGINA
-			// OFFSET RETORNA O TOP E O LEFT DO ELEMENTO
-			// A ALTURA DO MENU É FIXA APÓS A ROLAGEM DEVIDO A FUNÇÃO DIMINUIR NAVBAR
-			targetOffset = $(id).offset().top,
-			menuHeight = 100
-			
+        var id = $(this).attr('href'),
+            // PEGA A DISTÂNCIA ENTRE O ELEMENTO CAPTURADO E O TOPO DA PÁGINA
+            // OFFSET RETORNA O TOP E O LEFT DO ELEMENTO
+            // A ALTURA DO MENU É FIXA APÓS A ROLAGEM DEVIDO A FUNÇÃO DIMINUIR NAVBAR
+            targetOffset = $(id).offset().top,
+            menuHeight = 100
 
-		// SELECIONO O HTML E O BODY PARA ANIMAR
-		$('html, body').animate({
-			// SCROLLTOP ANIMA O DESLIZE
-			// 500 É A QUANTIDADE DE TEMPO EM MS
-			// É NECESSÁRIO SUBTRAIR O TAMANHO DO MENU PARA A DESCIDA SER AJUSTADA
-			// +1 SERVE PARA FAZER O AJUESTE IDEAL DA LINHA
-			
-			scrollTop: (targetOffset - menuHeight) + 1
-		}, 500)
-	});
+
+        // SELECIONO O HTML E O BODY PARA ANIMAR
+        $('html, body').animate({
+            // SCROLLTOP ANIMA O DESLIZE
+            // 500 É A QUANTIDADE DE TEMPO EM MS
+            // É NECESSÁRIO SUBTRAIR O TAMANHO DO MENU PARA A DESCIDA SER AJUSTADA
+            // +1 SERVE PARA FAZER O AJUESTE IDEAL DA LINHA
+
+            scrollTop: (targetOffset - menuHeight) + 1
+        }, 500)
+    });
 }
 
 anima_rolagem()
 
 
 // FUNÇÃO ATIVA A COR DO NAVBAR QUANDO A PÁGINA É ROLADA PARA BAIXO
-	// PEGA A ATIVAÇÃO DO SCROLL E FAZ UMA ADIÇÃO DE CLASSE CASO ESTEJA ROLANDO E REMOVE QUANDO VOLTA AO TOPO 
-function cor_navbar(){
-	
-	$(window).on('scroll', function(){
+// PEGA A ATIVAÇÃO DO SCROLL E FAZ UMA ADIÇÃO DE CLASSE CASO ESTEJA ROLANDO E REMOVE QUANDO VOLTA AO TOPO 
+function cor_navbar() {
 
-		if ($(window).scrollTop()){
-			$('nav').addClass('cor_menu');
+    $(window).on('scroll', function() {
 
-		}
-		else {
-			$('nav').removeClass('cor_menu');
-		}
-	})
+        if ($(window).scrollTop()) {
+            $('nav').addClass('cor_menu');
 
-	
+        } else {
+            $('nav').removeClass('cor_menu');
+        }
+    })
+
+
 }
 
 // MUDA A COR QUANDO O MENU ABRE
-function icone_mudar_cor(){
-	if ($('nav button').click(function(e){
-		$('nav').removeClass('small').addClass('cor_menu');	
-	}));
+function icone_mudar_cor() {
+    if ($('nav button').click(function(e) {
+            $('nav').removeClass('small').addClass('cor_menu');
+        }));
 
 }
-	
+
 icone_mudar_cor();
 cor_navbar()
 
 // CARREGA CLASSE QUE DIMINUI O TAMANHO DO MENU
-function diminur_navbar(){
-	
-	$(window).on('scroll', function(){
+function diminur_navbar() {
 
-		if ($(window).scrollTop()){
-			$('nav').addClass('small');
+    $(window).on('scroll', function() {
 
-		}
-		else {
-			$('nav').removeClass('small');
-		}
-	})
+        if ($(window).scrollTop()) {
+            $('nav').addClass('small');
+
+        } else {
+            $('nav').removeClass('small');
+        }
+    })
 
 }
 // FECHA O MENU APÓS ALGUM ITEM FOR CLICADO
-function fecha_menu(){
-	var menu = document.getElementsByClassName("navbar-collapse");
-	var icone_menu = document.getElementsByClassName("navbar-toggler");
-	if ($(menu).click(function(e){
-		$(menu).removeClass('show');
-		$(icone_menu).addClass('collapsed');
-		$(icone_menu).attr("aria-expanded", "false")
-	}));
+function fecha_menu() {
+    var menu = document.getElementsByClassName("navbar-collapse");
+    var icone_menu = document.getElementsByClassName("navbar-toggler");
+    if ($(menu).click(function(e) {
+            $(menu).removeClass('show');
+            $(icone_menu).addClass('collapsed');
+            $(icone_menu).attr("aria-expanded", "false")
+        }));
 
 
 }
@@ -95,57 +93,94 @@ fecha_menu()
 // DEBOUNCE DO LODASH
 // FUNÇÃO USADA DENTRO DO FRAMEWORK LODASH
 // ELA SEGURA A ANIMAÇÃO DA ANIMACAO_COMSCROLL POR ALGUNS SEGUNDOS PARA MELHORAR A PERFORMANCE DO SITE
-debounce = function(func, wait, immediate){
-	var timeout;
-	return function(){
-		var context = this, args = arguments;
-		var later = function(){
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
+debounce = function(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this,
+            args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
 };
 
 // FUNÇÃO DA ANIMAÇÃO COM O SCROLL
-function animacao_comscroll_left(){
+function animacao_comscroll_left() {
 
-	// CAPTURA A CLASSE SCROLL ANIMADO
-	var $target = $('.scroll_animado'),
-		// CAPTURA A CLASSE SCROLL ANIMADO START
-		animationClass = 'scroll_animado_start',
-		offset = $(window).height() * 3/4;
+    // CAPTURA A CLASSE SCROLL ANIMADO
+    var $target = $('.scroll_animado'),
+        // CAPTURA A CLASSE SCROLL ANIMADO START
+        animationClass = 'scroll_animado_start',
+        offset = $(window).height() * 3 / 4;
 
-	// FUNÇÃO QUE FAZ A ANIMAÇÃO ACONTECER
-	function animeScroll(){
-		// FUNÇÃO SCROLLTOP RETORNA O VALOR DA DISTÂNCIA DA POSIÇÃO DO SCROLL ATÉ O TOPO DA PÁGINA
-		var documentTop = $(document).scrollTop();
-		
-		// TARGET EACH PEGA A DISTÂNCIA DE CADA ITEM E O ANIMA
-		$target.each(function(){
-			// PEGA A DIST ENTRE O ITEM E O TOPO
-			// THIS ESTÁ SENDO REFERENTE A SCROLL ANIMADO
-			var itemTop = $(this).offset().top;
-			// SE A DIST DO DOCUMENTO ATÉ O TOPO FOR MAIOR QUE A DIST DO ITEM ATÉ O TOPO ELE ADICIONA A ANIMAÇÃO
-			if (documentTop > itemTop - offset){
-				$(this).addClass(animationClass);	
-			}
-			else{
-				$(this).removeClass(animationClass);
-			}
-		})
+    // FUNÇÃO QUE FAZ A ANIMAÇÃO ACONTECER
+    function animeScroll() {
+        // FUNÇÃO SCROLLTOP RETORNA O VALOR DA DISTÂNCIA DA POSIÇÃO DO SCROLL ATÉ O TOPO DA PÁGINA
+        var documentTop = $(document).scrollTop();
 
-	}
-	animeScroll();
+        // TARGET EACH PEGA A DISTÂNCIA DE CADA ITEM E O ANIMA
+        $target.each(function() {
+            // PEGA A DIST ENTRE O ITEM E O TOPO
+            // THIS ESTÁ SENDO REFERENTE A SCROLL ANIMADO
+            var itemTop = $(this).offset().top;
+            // SE A DIST DO DOCUMENTO ATÉ O TOPO FOR MAIOR QUE A DIST DO ITEM ATÉ O TOPO ELE ADICIONA A ANIMAÇÃO
+            if (documentTop > itemTop - offset) {
+                $(this).addClass(animationClass);
+            } else {
+                $(this).removeClass(animationClass);
+            }
+        })
 
-	// FUNÇÃO QUE CAPTURA O SCROLL
-	// ENCAPSULEI A FUNÇÃO COM O DEBOUNCE QUE DÁ O ATRASO DE 80 MS
-	$(document).scroll(debounce(function(){
-		animeScroll();
-	}, 80));
+    }
+    animeScroll();
+
+    // FUNÇÃO QUE CAPTURA O SCROLL
+    // ENCAPSULEI A FUNÇÃO COM O DEBOUNCE QUE DÁ O ATRASO DE 80 MS
+    $(document).scroll(debounce(function() {
+        animeScroll();
+    }, 80));
 };
 
 animacao_comscroll_left();
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
--- conteudo/secao_pratos.php
+++ conteudo/secao_pratos.php
@@ -1,94 +1,40 @@
-<div class="scroll_animado">
-	<div class="secao_pratos " id="pratos">
-		<h1 class="pt-4 pb-4">Principais Pratos</h1>
-		<section class="gallery-block compact-gallery">  		
-    		<div class="row no-gutters">
-    			<div class="col-md-6 col-lg-4 item zoom-on-hover">
-    				<a class="lightbox" href="midia/banner.jpg">
-    					<img class="img-fluid image" src="midia/banner.jpg">
-    					<span class="description">
-    						<span class="description-heading">Lorem Ipsum</span>
-    					</span>
-    				</a>
-    			</div>
-    			
-    			
-    			<div class="col-md-6 col-lg-4 item zoom-on-hover">
-    				<a class="lightbox" href="midia/banner.jpg">
-    					<img class="img-fluid image" src="midia/banner.jpg">
-    					<span class="description">
-    						<span class="description-heading">Lorem Ipsum</span>
-    					</span>
-    				</a>
-    			</div>
-    			
-    			
-    			<div class="col-md-6 col-lg-4 item zoom-on-hover">
-    				<a class="lightbox" href="midia/banner.jpg">
-    					<img class="img-fluid image" src="midia/banner.jpg">
-    					<span class="description">
-    						<span class="description-heading">Lorem Ipsum</span>
-    					</span>
-    				</a>
-    			</div>
-    			
-    			<div class="col-md-6 col-lg-4 item zoom-on-hover">
-    				<a class="lightbox" href="midia/banner.jpg">
-    					<img class="img-fluid image" src="midia/banner.jpg">
-    					<span class="description">
-    						<span class="description-heading">Lorem Ipsum</span>
-    					</span>
-    				</a>
-    			</div>
-    			
-    			
-    			<div class="col-md-6 col-lg-4 item zoom-on-hover">
-    				<a class="lightbox" href="midia/banner.jpg">
-    					<img class="img-fluid image" src="midia/banner.jpg">
-    					<span class="description">
-    						<span class="description-heading">Lorem Ipsum</span>
-    					</span>
-    				</a>
-    			</div>
-    			
-    			
-    			<div class="col-md-6 col-lg-4 item zoom-on-hover">
-    				<a class="lightbox" href="midia/banner.jpg">
-    					<img class="img-fluid image" src="midia/banner.jpg">
-    					<span class="description">
-    						<span class="description-heading">Lorem Ipsum</span>
-    					</span>
-    				</a>
-    			</div>
-    			
-    			<div class="col-md-6 col-lg-4 item zoom-on-hover">
-    				<a class="lightbox" href="midia/banner.jpg">
-    					<img class="img-fluid image" src="midia/banner.jpg">
-    					<span class="description">
-    						<span class="description-heading">Lorem Ipsum</span>
-    					</span>
-    				</a>
-    			</div>
-    			
-    			<div class="col-md-6 col-lg-4 item zoom-on-hover">
-    				<a class="lightbox" href="midia/banner.jpg">
-    					<img class="img-fluid image" src="midia/banner.jpg">
-    					<span class="description">
-    						<span class="description-heading">Lorem Ipsum</span>
-    					</span>
-    				</a>
-    			</div>
-    			
-    			<div class="col-md-6 col-lg-4 item zoom-on-hover">
-    				<a class="lightbox" href="midia/banner.jpg">
-    					<img class="img-fluid image" src="midia/banner.jpg">
-    					<span class="description">
-    						<span class="description-heading">Lorem Ipsum</span>
-    					</span>
-    				</a>
-    			</div>
-    		</div>
-    	</section>
-	</div>
+<div class="row">
+  <div class="column">
+    <img src="midia\lightbox\prato1.jpg" style="width:100%" onclick="openModal();currentSlide(1)" class="hover-shadow cursor">
+  </div>
+  <div class="column">
+    <img src="midia\lightbox\prato2.jpg" style="width:100%" onclick="openModal();currentSlide(2)" class="hover-shadow cursor">
+  </div>
+  <div class="column">
+    <img src="midia\lightbox\prato3.jpg" style="width:100%" onclick="openModal();currentSlide(3)" class="hover-shadow cursor">
+  </div>
+</div>
 
+<div id="myModal" class="modal">
+  <span class="close cursor" onclick="closeModal()">&times;</span>
+  <div class="modal-content">
+
+    <div class="mySlides">
+      <div class="numbertext">1 / 4</div>
+      <img src="midia\lightbox\prato1.jpg" style="width:100%">
+    </div>
+
+    <div class="mySlides">
+      <div class="numbertext">2 / 4</div>
+      <img src="midia\lightbox\prato2.jpg" style="width:100%">
+    </div>
+
+    <div class="mySlides">
+      <div class="numbertext">3 / 3</div>
+      <img src="midia\lightbox\prato3.jpg" style="width:100%">
+    </div>
+    
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+    <div class="caption-container">
+      <p id="caption"></p>
+    </div>
+	
+  </div>
 </div>
