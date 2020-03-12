e8067d98cc1734a7c4886f2dda94c0cc35ed1ba9
StellaPizani <pizanistella14@gmail.com>
2020-03-12 20:46:38 -0300

    Update secao_pratos.php

--- conteudo/secao_pratos.php
+++ conteudo/secao_pratos.php
@@ -1,13 +1,18 @@
 <div class="container">
-  <div class="row">
-    <div class="col order-last">
-    <a data-fancybox="gallery" href="midia\lightbox\prato1.jpg"><img src="midia\lightbox\thumb_prato1.png"></a>
+    <div id="pratos" class=" scroll_animado" >
+          <div class="secao_pratos" >
+            <h1 class="pb-4">Pratos</h1>
+          <div class="row">
+            <div class="col-sm-4 order-last">
+            <a data-fancybox="gallery" href="midia\lightbox\prato1.jpg"><img src="midia\lightbox\prato1.jpg" class="img-fluid"></a>
+            </div>
+            <div class="col-sm-4 ">
+            <a data-fancybox="gallery" href="midia\lightbox\prato2.jpg"><img src="midia\lightbox\prato2.jpg" class="img-fluid"></a>
+            </div>
+            <div class="col-sm-4  order-first">
+            <a data-fancybox="gallery" href="midia\lightbox\prato3.jpg"><img src="midia\lightbox\prato3.jpg" class="img-fluid"></a>
+            </div>
+          </div>
+        </div>
     </div>
-    <div class="col">
-    <a data-fancybox="gallery" href="midia\lightbox\prato2.jpg"><img src="midia\lightbox\thumb_prato2.png"></a>
-    </div>
-    <div class="col order-first">
-    <a data-fancybox="gallery" href="midia\lightbox\prato3.jpg"><img src="midia\lightbox\thumb_prato3.png"></a>
-    </div>
-  </div>
 </div>
