66e450fd67d271025b2a430bccc872e2deaecade
StellaPizani <pizanistella14@gmail.com>
2020-03-12 21:12:02 -0300

    f

--- conteudo/secao_pratos.php
+++ conteudo/secao_pratos.php
@@ -3,14 +3,14 @@
           <div class="secao_pratos" >
             <h1 class="pb-4">Pratos</h1>
           <div class="row">
-            <div class="col-sm-4 order-last">
-            <a data-fancybox="gallery" href="midia\lightbox\prato1.jpg"><img src="midia\lightbox\prato1.jpg" class="img-fluid"></a>
+            <div class="col-sm-4 order-first">
+                <a data-fancybox="gallery" href="midia\lightbox\prato1.jpg"><img src="midia\lightbox\prato1.jpg" class="img-fluid"></a>
             </div>
             <div class="col-sm-4 ">
-            <a data-fancybox="gallery" href="midia\lightbox\prato2.jpg"><img src="midia\lightbox\prato2.jpg" class="img-fluid"></a>
+                <a data-fancybox="gallery" href="midia\lightbox\prato2.jpg"><img src="midia\lightbox\prato2.jpg" class="img-fluid"></a>
             </div>
-            <div class="col-sm-4  order-first">
-            <a data-fancybox="gallery" href="midia\lightbox\prato3.jpg"><img src="midia\lightbox\prato3.jpg" class="img-fluid"></a>
+            <div class="col-sm-4  order-last ">
+                <a data-fancybox="gallery" href="midia\lightbox\prato3.jpg"><img src="midia\lightbox\prato3.jpg" class="img-fluid"></a>
             </div>
           </div>
         </div>
