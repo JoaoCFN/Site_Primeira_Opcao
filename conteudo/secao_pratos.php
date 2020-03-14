bb02346851c6f123a5d6462804cfe5063aea927a
Genivaldo Alves <geniinho@hotmail.com>
2020-03-14 07:28:20 -0300

    pratos atualizado

--- conteudo/secao_pratos.php
+++ conteudo/secao_pratos.php
@@ -4,14 +4,40 @@
             <h1 class="pb-4">Pratos</h1>
           <div class="row">
             <div class="col-sm-4 order-first">
-                <a data-fancybox="gallery" href="midia\lightbox\prato1.jpg"><img src="midia\lightbox\prato1.jpg" class="img-fluid"></a>
+                <a data-fancybox="gallery" href="midia\lightbox\imagem2.jpg"><img src="midia\lightbox\thumb\thumb (1).jpg" class="img-fluid"></a>
             </div>
             <div class="col-sm-4 ">
-                <a data-fancybox="gallery" href="midia\lightbox\prato2.jpg"><img src="midia\lightbox\prato2.jpg" class="img-fluid"></a>
+                <a data-fancybox="gallery" href="midia\lightbox\imagem3.jpg"><img src="midia\lightbox\thumb\thumb (2).jpg" class="img-fluid"></a>
             </div>
+
+            <div class="col-sm-4 ">
+                <a data-fancybox="gallery" href="midia\lightbox\imagem4.jpg"><img src="midia\lightbox\thumb\thumb (3).jpg" class="img-fluid"></a>
+            </div>
+
+            <div class="col-sm-4 ">
+                <a data-fancybox="gallery" href="midia\lightbox\imagem5.jpg"><img src="midia\lightbox\thumb\thumb (4).jpg" class="img-fluid"></a>
+            </div>
+
+            <div class="col-sm-4 ">
+                <a data-fancybox="gallery" href="midia\lightbox\imagem6.jpg"><img src="midia\lightbox\thumb\thumb (5).jpg" class="img-fluid"></a>
+            </div>
+
+            <div class="col-sm-4 ">
+                <a data-fancybox="gallery" href="midia\lightbox\imagem7.jpg"><img src="midia\lightbox\thumb\thumb (6).jpg" class="img-fluid"></a>
+            </div>
+
+            <div class="col-sm-4 ">
+                <a data-fancybox="gallery" href="midia\lightbox\imagem8.jpg"><img src="midia\lightbox\thumb\thumb (7).jpg" class="img-fluid"></a>
+            </div>
+
             <div class="col-sm-4  order-last ">
-                <a data-fancybox="gallery" href="midia\lightbox\prato3.jpg"><img src="midia\lightbox\prato3.jpg" class="img-fluid"></a>
+                <a data-fancybox="gallery" href="midia\lightbox\imagem9.jpg"><img src="midia\lightbox\thumb\thumb (8).jpg" class="img-fluid"></a>
             </div>
+
+            <div class="col-sm-4  order-last ">
+                <a data-fancybox="gallery" href="midia\lightbox\imagem10.jpg"><img src="midia\lightbox\thumb\thumb (9).jpg" class="img-fluid"></a>
+            </div>
+
           </div>
         </div>
     </div>
