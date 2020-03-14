58aed9a870eb96387ab7444dcf8070beaf6f2dc0
João Carlos Ferreira Neto <58020444+JoaoCFN@users.noreply.github.com>
2020-03-13 23:57:45 -0300

    Seção pratos
    Falta implementar os textos e padronizar os ícones

--- conteudo/secao_pratos.php
+++ conteudo/secao_pratos.php
@@ -7,7 +7,7 @@
                 <a data-fancybox="gallery" href="midia\lightbox\prato1.jpg"><img src="midia\lightbox\prato1.jpg" class="img-fluid"></a>
             </div>
             <div class="col-sm-4 ">
-                <a data-fancybox="gallery" href="midia\lightbox\prato2.jpg"><img src="midia\lightbox\prato2.jpg" class="img-fluid"></a>
+                <a data-fancybox="gallery" href="midia\lightbox\prato1.jpg"><img src="midia\lightbox\prato1.jpg" class="img-fluid"></a>
             </div>
             <div class="col-sm-4  order-last ">
                 <a data-fancybox="gallery" href="midia\lightbox\prato3.jpg"><img src="midia\lightbox\prato3.jpg" class="img-fluid"></a>
