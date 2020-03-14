98b51dd26865cb97f42b3f1d0d7ed66e6e9d1f1b
João Carlos Ferreira Neto <58020444+JoaoCFN@users.noreply.github.com>
2020-03-14 13:34:33 -0300

    dd

--- conteudo/secao_servicos.php
+++ conteudo/secao_servicos.php
@@ -1,16 +1,16 @@
 <div class="secao_servicos">
 	<div class="scroll_animado pt-3" id="servicos">
-		<h1 class="txt_capa_4 pb-3">Serviços</h1>
+		<h1 class="txt_capa_4">Serviços</h1>
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-4 align-self-center">
-					<img src="./midia/servicos/icon1.png" class="img-fluid" align="center" width="300">	
+					<img src="./midia/servicos/icon1.png" class="img-fluid" align="center"> 	
 				</div>
 				<div class="col-sm-4 align-self-center" class="img-fluid">
-					<img src="./midia/servicos/icon2.png" class="img-fluid" align="center" width="350"> 	
+					<img src="./midia/servicos/icon2.png" class="img-fluid" align="center"> 	
 				</div>
 				<div class="col-sm-4 align-self-center" class="img-fluid">
-					<img src="./midia/servicos/icon3.png" class="img-fluid " align="center" width="350">	
+					<img src="./midia/servicos/icon3.png" class="img-fluid " align="center">	
 				</div>	
 			</div>	
 		</div>
