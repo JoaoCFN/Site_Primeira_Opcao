7c97d1541f525a7822f1662f2041a5e15644baf0
João Carlos Ferreira Neto <58020444+JoaoCFN@users.noreply.github.com>
2020-03-15 23:01:59 -0300

    Adição do texto da parte de serviços

--- conteudo/secao_servicos.php
+++ conteudo/secao_servicos.php
@@ -2,18 +2,27 @@
 	<div class="scroll_animado pt-3" id="servicos">
 		<h1 class="txt_capa_4 pb-4">Serviços</h1>
 		<div class="container">
-			<div class="row">
-				<div class="col-sm-4 align-self-center" align="center">
-					<img src="./midia/servicos/icone1.png" class="img-fluid icones_servicos">
-					<h5 class="pb-4">Alimentos de dieta</h5> 	
-				</div>
+			<div class="row">		
 				<div class="col-sm-4 align-self-center" class="img-fluid" align="center">
 					<img src="./midia/servicos/icone2.png" class="img-fluid icones_servicos"> 
-					<h5 class="pb-4">Cozinha Industrial</h5> 	
+					<h5>Cozinha Industrial</h5> 	
+					<p class="pb-4">
+						Fornecemos o nosso trabalho ao seu estabelecimento. Preparamos refeições de acordo com a cultura e hábitos locais.	
+					</p>	
+				</div>
+				<div class="col-sm-4 align-self-center" align="center">
+					<img src="./midia/servicos/icone1.png" class="img-fluid icones_servicos">
+					<h5>Alimentos de dieta</h5> 
+					<p class="pb-4">
+						Temos profissionais especializados que preparam sua dieta personalizada com base nos seus desejos e restrições. 
+					</p>		
 				</div>
 				<div class="col-sm-4 align-self-center" class="img-fluid" align="center">
 					<img src="./midia/servicos/icone3.png" class="img-fluid icones_servicos">
-					<h5 class="pb-4">Buffet</h5>	
+					<h5>Buffet</h5>
+					<p class="pb-4">
+						Servimos nossos alimentos também em eventos, e garantimos total qualidade no que diz respeito ao sabor e a quantidade.		
+					</p>	
 				</div>	
 			</div>	
 		</div>
